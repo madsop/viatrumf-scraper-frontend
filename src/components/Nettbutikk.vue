f4cb3e1d76d013fb6fa6ba907d2b8692dafc566b
Mads Opheim <mads.opheim@gmail.com>
2020-03-26 20:52:42 +0100

    Skjelett, første versjon

---
+++ src/components/Nettbutikk.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Nettbutikk</h1>
+        {{ namn }}
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Nettbutikk extends Vue {
+  @Prop() private namn!: string;
+
+  public setNamn(namn: string) {
+      this.namn = namn;
+  }
+}
+</script>
---
+++ src/components/Nettbutikker.vue
@@ -0,0 +1,32 @@
+-<template>
+    <div>
+        <h1>Nettbutikker</h1>
+        <section v-for="nettbutikk in nettbutikker" :key="nettbutikk.namn">
+            <Nettbutikk :namn=nettbutikk.namn />
+        </section>
+    </div>
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-property-decorator';
+import Nettbutikk from '@/components/Nettbutikk.vue';
+
+export default Vue.extend({
+    components: {
+        Nettbutikk,
+    },
+    data: () => ({
+        nettbutikker: [] as Nettbutikk[],
+    }),
+    methods: {
+        addNettbutikk(nettbutikk: Nettbutikk) {
+            this.nettbutikker.push(nettbutikk);
+        }
+    },
+    mounted() {
+        const nettbutikk1 = new Nettbutikk();
+        nettbutikk1.setNamn('Hotels.com');
+        this.addNettbutikk(nettbutikk1);
+    }
+});
+</script>
