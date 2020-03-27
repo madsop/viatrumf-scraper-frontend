bd82892939031a67b7b89cfe502ee75825a17195
Mads Opheim <mads.opheim@gmail.com>
2020-03-27 23:14:13 +0100

    Koplar opp mot backend. Hentar alle nettbutikkar og visar fram i nedtrekksliste

--- src/components/Nettbutikk.vue
+++ src/components/Nettbutikk.vue
@@ -1,19 +1,38 @@
 <template>
     <div>
-        <h1>Nettbutikk</h1>
         {{ namn }}
+        <table>
+        <div v-for="enkeltinnslag in innslag" :key="enkeltinnslag.timestamp">
+                <tr>
+                    <td>{{ enkeltinnslag.timestamp | formatTime }}:</td>
+                    <td><strong>{{enkeltinnslag.verdi }}</strong></td>
+                    <td>({{ enkeltinnslag.kategori }})</td>
+                </tr>
+        </div>
+        </table>
     </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
+import moment from 'moment';
 
 @Component
 export default class Nettbutikk extends Vue {
   @Prop() private namn!: string;
+  @Prop() private innslag!: [];
 
   public setNamn(namn: string) {
       this.namn = namn;
   }
+
+  public setInnslag(innslag: []) {
+      this.innslag = innslag;
+  }
 }
+Vue.filter('formatTime', function(timestamp: string) {
+    moment.locale
+    if (!timestamp) return;
+    return moment(timestamp, 'YYYYMMDDTHHmmssZ').format('LLL'); 
+})
 </script>
--- src/components/Nettbutikker.vue
+++ src/components/Nettbutikker.vue
@@ -1,14 +1,19 @@
 -<template>
     <div>
         <h1>Nettbutikker</h1>
-        <section v-for="nettbutikk in nettbutikker" :key="nettbutikk.namn">
-            <Nettbutikk :namn=nettbutikk.namn />
-        </section>
+        <select v-model="selected" v-on:change="selectionChanged">
+            <option selected="selected">Velg nettbutikk</option>
+            <option v-for="nettbutikk in nettbutikker" :key="nettbutikk.namn" v-bind:value="nettbutikk">
+                {{ nettbutikk }} 
+            </option>
+        </select>
+        <Nettbutikk :namn=selected :innslag=selectedNettbutikk />
     </div>
 </template>
 
 <script lang="ts">
 import { Vue } from 'vue-property-decorator';
+import axios from 'axios';
 import Nettbutikk from '@/components/Nettbutikk.vue';
 
 export default Vue.extend({
@@ -16,17 +21,24 @@ export default Vue.extend({
         Nettbutikk,
     },
     data: () => ({
+        baseurl: 'http://localhost:8081',
         nettbutikker: [] as Nettbutikk[],
+        selected: '',
+        selectedNettbutikk: [],
     }),
     methods: {
-        addNettbutikk(nettbutikk: Nettbutikk) {
-            this.nettbutikker.push(nettbutikk);
-        }
+        selectionChanged() {
+            const self = this;
+            axios.get(self.baseurl + '/nettbutikkar/' + self.selected)
+        .then((response) => response.data)
+        .then((data) => self.selectedNettbutikk = data);
+        },
     },
     mounted() {
-        const nettbutikk1 = new Nettbutikk();
-        nettbutikk1.setNamn('Hotels.com');
-        this.addNettbutikk(nettbutikk1);
+        const self = this;
+        axios.get(self.baseurl + '/nettbutikkar')
+        .then((response) => response.data)
+        .then((data) => self.nettbutikker = data);
     }
 });
 </script>
