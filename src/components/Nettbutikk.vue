ae769a81e20a83e2feb695c7704b1f6e5cb34039
Mateusz <mz@computas.com>
2020-03-31 22:50:37 +0200

    Masse ts endringen men fungerer fortsatt

--- src/components/Nettbutikk.vue
+++ src/components/Nettbutikk.vue
@@ -1,64 +1,68 @@
 <template>
-    <div>
-        <h1>{{ namn |removeUnderscore }}</h1>
-        <div v-if="innslag.length > 0">
-            {{ innslag[0].kategori | capitalizeFirst }}
-            <hr />
-            <a  :href="'https://viatrumf.no/' + innslag[0].href">Til Viatrumf-sida</a>
-            <LineChart 
-                :labels=this.labels
-                :data=this.data
-            />
-        </div>
-        <table>
-            <tr v-for="enkeltinnslag in innslag" :key="enkeltinnslag.timestamp">
-                <td>{{ enkeltinnslag.timestamp | formatTime }}</td>
-                <td><strong>{{enkeltinnslag.verdi }}</strong></td>
-            </tr>
-        </table>
+  <div>
+    <h1>{{ namn | removeUnderscore }}</h1>
+    <div v-if="innslag.length > 0">
+      {{ innslag[0].kategori | capitalizeFirst }}
+      <hr />
+      <a :href="'https://viatrumf.no/' + innslag[0].href">Til Viatrumf-sida</a>
+      <LineChart :labels="labels" :data="datapunkter" />
     </div>
+    <table>
+      <tr v-for="enkeltinnslag in innslag" :key="enkeltinnslag.timestamp">
+        <td>{{ enkeltinnslag.timestamp | formatTime }}</td>
+        <td>
+          <strong>{{ enkeltinnslag.verdi }}</strong>
+        </td>
+      </tr>
+    </table>
+  </div>
 </template>
 
 <script lang="ts">
+import { Vue, Component, Prop, Watch } from 'vue-property-decorator'
+import LineChart from '@/components/NettbutikkChart.vue'
+import INettbutikkInnslag from '@/model/INettbutikkInnslag'
 
-import LineChart from '@/components/NettbutikkChart.vue';
-import { Vue} from 'vue-property-decorator'; 
+@Component({
+  components: {
+    LineChart
+  }
+})
+export default class Nettbutikk extends Vue {
+  @Prop() namn!: string
+  @Prop() innslag!: INettbutikkInnslag[]
 
-export default {
-    extends: Vue,
-    components: {
-        LineChart
-    },
-    props: ['namn', 'innslag'],
-    methods: {
-        trim(input: string): string {
-            return input.replace(/%/g, '').replace(/kr/g, '',).replace(' ', '').replace(',', '.');
-        }
-    },
-    computed: {
-        labels(): string[] {
-            const label = [] as string[];
-            (this as any).innslag.forEach((e: any) => {
-                label.push(e.timestamp);
-            });
-            return label;
-        },
-        data(): string[] {
-            const datapunkt = [] as string[];
-            const self = (this as any);
-            self.innslag.forEach((e: any) => {
-                datapunkt.push(self.trim((e.verdi as string)));
-            });
-            return datapunkt;
-        }
-    },
+  private trim(input: string): string {
+    return input
+      .replace(/%/g, '')
+      .replace(/kr/g, '')
+      .replace(' ', '')
+      .replace(',', '.')
+  }
+
+  get labels(): string[] {
+    const labels: string[] = []
+
+    this.innslag.forEach((enkeltInnslag: INettbutikkInnslag) => {
+      labels.push(enkeltInnslag.timestamp)
+    })
+    return labels
+  }
+
+  get datapunkter(): string[] {
+    const datapunkter: string[] = []
+
+    this.innslag.forEach((enkeltInnslag: INettbutikkInnslag) => {
+      datapunkter.push(this.trim(enkeltInnslag.verdi))
+    })
+    return datapunkter
+  }
 }
-
 </script>
 
 <style scoped>
 td {
-    width: 200px;
-    padding: 0.5em 1em;
+  width: 200px;
+  padding: 0.5em 1em;
 }
 </style>
--- src/components/Nettbutikker.vue
+++ src/components/Nettbutikker.vue
@@ -1,52 +1,50 @@
 -<template>
-    <div>
-        <h1>Historikk for nettbutikkar på Viatrumf</h1>
-        <p>Usikker på om Viatrumf-bonusen på nettbutikken du vil handle hos har gått opp, ned eller er uendra? Sjekk ved å velje butikken i nedtrekkslista.</p> 
-        <select v-model="selected" v-on:change="selectionChanged">
-            <option value="" selected disabled>Velg nettbutikk</option>
-            <option v-for="nettbutikk in nettbutikker" :key="nettbutikk.namn" v-bind:value="nettbutikk">
-                {{ nettbutikk | removeUnderscore }} 
-            </option>
-        </select>
-        <Nettbutikk :namn=selected :innslag=selectedNettbutikk />
-    </div>
+  <div>
+    <h1>Historikk for nettbutikkar på Viatrumf</h1>
+    <p>
+      Usikker på om Viatrumf-bonusen på nettbutikken du vil handle hos har gått
+      opp, ned eller er uendra? Sjekk ved å velje butikken i nedtrekkslista.
+    </p>
+    <select v-model="selected" v-on:change="selectionChanged">
+      <option value="" selected disabled>Velg nettbutikk</option>
+      <option
+        v-for="nettbutikk in nettbutikker"
+        :key="nettbutikk.namn"
+        v-bind:value="nettbutikk"
+      >
+        {{ nettbutikk | removeUnderscore }}
+      </option>
+    </select>
+    <Nettbutikk :namn="selected" :innslag="selectedNettbutikk" />
+  </div>
 </template>
 
 <script lang="ts">
-import { Vue } from 'vue-property-decorator';
-import axios from 'axios';
-import Nettbutikk from '@/components/Nettbutikk.vue';
+import { Vue, Component } from 'vue-property-decorator'
+import axios from 'axios'
+import Nettbutikk from '@/components/Nettbutikk.vue'
 
-export default Vue.extend({
-    components: {
-        Nettbutikk,
-    },
-    data: () => ({
-        baseurl: 'https://viatrumf-scraper-bff-xgjgr2ubbq-ew.a.run.app',
-        nettbutikker: [],
-        selected: '',
-        selectedNettbutikk: [],
-    }),
-    methods: {
-        selectionChanged() {
-            const self = this;
-            axios.get(self.baseurl + '/nettbutikkar/' + self.selected)
-        .then((response) => response.data)
-        .then((data) => self.selectedNettbutikk = data);
-        },
-    },
-    mounted() {
-        const self = this;
-        axios.get(self.baseurl + '/nettbutikkar')
-        .then((response) => response.data)
-        .then((data) => self.nettbutikker = data);
-    }
-});
-</script>
+@Component({
+  components: {
+    Nettbutikk
+  }
+})
+export default class Nettbutikker extends Vue {
+  private baseurl = 'https://viatrumf-scraper-bff-xgjgr2ubbq-ew.a.run.app'
+  private nettbutikker = []
+  private selected = ''
+  private selectedNettbutikk = []
 
-<style scoped>
-ul, li {
-    margin: 0;
-    padding: 0;
+  selectionChanged(): void {
+    axios
+      .get(this.baseurl + '/nettbutikkar/' + this.selected)
+      .then(response => (this.selectedNettbutikk = response.data))
+  }
+
+  mounted() {
+    axios
+      .get(this.baseurl + '/nettbutikkar')
+      .then(response => (this.nettbutikker = response.data))
+  }
 }
-</style>
+</script>
